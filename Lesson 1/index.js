fed0c22389aad95de5cdcd5db1c4f142398b6575
efoster9 <98239176+efoster9@users.noreply.github.com>
2024-12-09 15:26:56 +0000

    Updated instructions

--- Lesson 1/index.js
+++ Lesson 1/index.js
@@ -4,6 +4,17 @@
 
 console.log('hello')
 
+// Step 3: in the terminal below type node index.js to run your code
 
-// in the terminal below type node index.js to run your code
+
+// Step 4: add a variable with your partner's name
+
+// Step 5: update the console log to print "Hello " + your partner's name
+
+console.log('hello')
+
+// Step 6: in the terminal below type node index.js to run your code
+
+
+
 
