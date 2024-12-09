2abc3f02e14a4b6a9e7e87a3264f7d339b8b9e52
efoster9 <98239176+efoster9@users.noreply.github.com>
2024-12-09 15:25:25 +0000

    first files

---
+++ Lesson 1/index.js
@@ -0,0 +1,9 @@
+// Step 1: add a variable with your name
+
+// Step 2: update the console log to print "Hello " + your name
+
+console.log('hello')
+
+
+// in the terminal below type node index.js to run your code
+
