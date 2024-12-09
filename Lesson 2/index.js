b683ea8fad87d4ad2f56953827695c627f2a4e47
efoster9 <98239176+efoster9@users.noreply.github.com>
2024-12-09 15:30:19 +0000

    Added Lesson 2

---
+++ Lesson 2/index.js
@@ -0,0 +1,20 @@
+// Step 1: add a variable with your name
+
+// Step 2: update the console log to print "Goodbye " + your name
+
+console.log('goodbye')
+
+// Step 3: in the terminal below type node index.js to run your code
+
+
+// Step 4: add a variable with your partner's name
+
+// Step 5: update the console log to print "Goodbye " + your partner's name
+
+console.log('goodbye')
+
+// Step 6: in the terminal below type node index.js to run your code
+
+
+
+
